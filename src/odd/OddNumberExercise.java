47a86356d9e300b102f787f48edc07052cb08a26
thalyshenrique7 <thalys_henrique@outlook.com>
2023-06-07 15:41:33 -0300

    fix odd exercise.

--- src/odd/OddNumberExercise.java
+++ src/odd/OddNumberExercise.java
@@ -2,6 +2,7 @@ package odd;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *  In a list with integers numbers, return a new list only with square of odd numbers
@@ -10,9 +11,10 @@ public class OddNumberExercise {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        int calculate = numbers.stream()
+        List<Integer> calculate = numbers.stream()
                 .filter(odd -> odd % 2 == 1)
-                .reduce(0, Integer::sum);
+                        .map(odd -> odd * odd)
+                                .collect(Collectors.toList());
 
         System.out.println(calculate);
     }
